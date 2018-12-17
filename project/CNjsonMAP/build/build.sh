9259c15fa45afc70e858d6c513ba6de237c94097
Miao Cai <miaocai@slu.edu>
2018-12-17 16:31:32 -0600

    add final project files before dinner

--- project/CNjsonMAP/build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -r ../dist
-
-node r.js -o config.js
-node optimize.js
-
-node r.js -o dataTool.js
--- project/app.R
+++ project/app.R
@@ -1,51 +1,133 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+load("SXtemp.Rdata")
 
-
+require(dplyr)
 library(shiny)
+library(shinydashboard)
+library(xts)
+library(dygraphs)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+cities = unique(SXtemp$city)
+counties = unique(SXtemp$county)
 
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
 
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
 
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+# SIDE BAR
+sidebar <- dashboardSidebar(
+
+
+  sidebarMenu(
+  menuItem("Temperature Trend", tabName = "TempTrend"),
+  selectInput("city", label = "City:",
+              choices = cities,
+              selected = "datong"),
+  selectInput("county", label = "County:",
+              choices = NULL),
+  dateRangeInput("daterange", "Date range:",
+                 start  = "2013-01-01",
+                 end    = "2017-11-28",
+                 min    = "2013-01-01",
+                 max    = "2017-11-28",
+                 format = "yy/mm/dd",
+                 separator = " - "),
+  checkboxGroupInput("temperature", "Temperature:",
+                     choices = c("Max" = "tmax",
+                                 "Average" = "tmed",
+                                 "Min" = "tmin"),
+                     selected = c("tmax", "tmed", "tmin"))#,
+  #menuItem("Temperature variation", tabName = "TempVariation")
+  ),
+  br(),
+  img(src='SLUlogo.png', style="display: block; margin-left: auto; margin-right: auto;", width = 180)
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# BODY
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "TempTrend",
+            fluidRow(
+              dygraphOutput("SXtrend"),
+              br(),
+              dygraphOutput("SXvariation")))
+  )
+)
 
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Analyzing Temperature Patterns in Shanxi, China",
+                  titleWidth = 470),
+  sidebar,
+  body
+)
+
+server <- function(input, output, session) {
+
+  observeEvent(input$city, {
+    updateSelectInput(session,'county',
+                      choices=unique(SXtemp$county[SXtemp$city==input$city]),
+                      selected = unique(SXtemp$county[SXtemp$city==input$city])[1])
+  })
+
+
+  fildata = reactive({
+    SXtemp %>%
+      filter(city == input$city,
+             county == input$county,
+             date >= input$daterange[1],
+             date <= input$daterange[2]) %>%
+      arrange(date)
+  })
+
+
+  output$SXtrend <- renderDygraph({
+
+    fildata() %>%
+      select(!!unlist(input$temperature)) %>%
+      xts(., order.by = fildata()$date) %>%
+      dygraph(main = paste("Temperatures trends at",
+                            input$county, "County, ", input$city,
+                            "City, Shanxi, China, ", input$daterange[1],
+                            "to", input$daterange[2],
+                           sep = " "),
+              y = "Temperature in Fahrenheits",
+              group = "temperat") %>%
+      dyAxis("x", drawGrid = FALSE) %>%
+      dyOptions(axisLineColor = "navy",
+                gridLineColor = "lightblue") %>%
+      dyLegend(width = 400)
+
+  })
+
+
+  output$SXvariation <- renderDygraph({
+
+    fildata() %>%
+      select(tvarv, tvarh) %>%
+      xts(., order.by = fildata()$date) %>%
+      dygraph(main = paste("Temperatures variation at",
+                           input$county, "County, ", input$city,
+                           "City, Shanxi, China, ", input$daterange[1],
+                           "to", input$daterange[2],
+                           sep = " "),
+              y = "Temperature variation in Fahrenheits",
+              group = "temperat") %>%
+      dySeries("tvarv", label = "Vertical variation", color = "orange") %>%
+      dySeries("tvarh", label = "Horizontal variation", color = "green") %>%
+      dyOptions(axisLineColor = "navy",
+                gridLineColor = "lightblue") %>%
+      dyAxis("x", drawGrid = FALSE) %>%
+      dyLegend(width = 400) %>%
+      dyRangeSelector()
+
+  })
+
+
+
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+
 
